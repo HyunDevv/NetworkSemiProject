a0be4d0f5a3456c0114711470cd7e77f2d45e263
wellofwater <cumulonim96@gmail.com>
2020-11-13 13:34:16 +0900

    Update carRear arduino(sensor)-client-can

--- CarRear/carRear/src/com/can/SendAndReceiveSerialCan.java
+++ CarRear/carRear/src/com/can/SendAndReceiveSerialCan.java
@@ -12,7 +12,7 @@ import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
 
-public class SendAndReceiveSerial implements SerialPortEventListener {
+public class SendAndReceiveSerialCan implements SerialPortEventListener {
 	private BufferedInputStream bin;
 	private InputStream in;
 	private OutputStream out;
@@ -23,7 +23,9 @@ public class SendAndReceiveSerial implements SerialPortEventListener {
 	private String rawCanID, rawTotal;
 	// private boolean start = false;
 
-	public SendAndReceiveSerial(String portName, boolean mode) {
+	
+	// Serial-Can 통신
+	public SendAndReceiveSerialCan(String portName, boolean mode) {
 		try {
 			if (mode == true) {
 				portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
@@ -41,7 +43,7 @@ public class SendAndReceiveSerial implements SerialPortEventListener {
 		if (portIdentifier.isCurrentlyOwned()) {
 			System.out.println("Error: Port is currently in use");
 		} else {
-			commPort = portIdentifier.open(this.getClass().getName(), 5000);
+			commPort = portIdentifier.open(this.getClass().getName(), 5001);
 			if (commPort instanceof SerialPort) {
 				serialPort = (SerialPort) commPort;
 				serialPort.addEventListener(this);
@@ -73,18 +75,6 @@ public class SendAndReceiveSerial implements SerialPortEventListener {
 		sendTread.start();
 	}
 
-	public void sendcan(String data) {
-		SendAndReceiveSerialCan srcan = new SendAndReceiveSerialCan("COM6", true);
-		
-		// sensor, data에 따라 전송
-		if(data.equals("0")) {
-			srcan.sendSerial("W2810003B010000000000005020", "10003B01");
-		}else if(data.equals("1")) {
-			srcan.sendSerial("W2810003B010000000000005021", "10003B01");
-		}else if(data.equals("2")) {
-			srcan.sendSerial("W2810003B010000000000005022", "10003B01");
-		}
-	}
 	private class SerialWriter implements Runnable {
 		String data;
 
@@ -153,7 +143,6 @@ public class SendAndReceiveSerial implements SerialPortEventListener {
 				String ss = new String(readBuffer);
 				System.out.println("Receive Low Data:" + ss + "||");
 
-				sendcan(ss.substring(0, 1));
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -196,27 +185,32 @@ public class SendAndReceiveSerial implements SerialPortEventListener {
 		
 		return result;
 	}
+	
+	public String sensorTemp() {
+		String result = "";
+		
+		// 온도 가상 데이터
+		Random rTemp = new Random();
+		int temp = rTemp.nextInt(10);
+		
+		if(temp < 2) {
+			// 저온
+			result = "0000000000005021";
+		}else if(temp > 7) {
+			// 고온
+			result = "0000000000005022";
+		}else {
+			// 상온
+			result = "0000000000005020";
+		}
+		
+		return result;
+	}
 
 	public static void main(String args[]) throws IOException {
-		SendAndReceiveSerial ss = new SendAndReceiveSerial("COM4", true);
-		
-		// total = W2810003B0100000000000050nn
-//		String code = "W";
-//		String type = "28";
-//		String canId = "10003B01";
-//		
-//		while(true) {
-//			// 임의값에 따라 데이터 전송
-//			ss.sendSerial(code + type + canId + ss.sensorPIR(), canId);
-//			
-//			try {
-//				Thread.sleep(1000);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		}
+		SendAndReceiveSerialCan ss = new SendAndReceiveSerialCan("COM4", true);
 	}
+
 }
 
 
