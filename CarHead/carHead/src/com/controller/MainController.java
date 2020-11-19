780522521ee7f738e45fc6baa31ed0db318280f9
wellofwater <cumulonim96@gmail.com>
2020-11-19 12:52:37 +0900

    Update carHead, carFront, carRear CANbus

--- CarHead/carHead/src/com/controller/MainController.java
+++ CarHead/carHead/src/com/controller/MainController.java
@@ -2,15 +2,16 @@ package com.controller;
 
 import java.io.IOException;
 
-import com.can.SendAndReceiveSerial;
+import com.can.SendAndReceiveSerialCan;
 import com.tcpip.Client;
 
 public class MainController {
 
 	public static void main(String[] args) {
-		SendAndReceiveSerial ss = new SendAndReceiveSerial("COM5", true);
-		Client client = new Client("192.168.35.37",5558);
-		ss.setClient(client);
+		SendAndReceiveSerialCan can = new SendAndReceiveSerialCan("COM4", true);
+		Client client = new Client("192.168.0.123",5558);
+		can.setClient(client);
+		client.setCan(can);
 		
 		try {
 			client.connect();
